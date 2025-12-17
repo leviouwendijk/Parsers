1efb0b12a755684d4cd52eec13bdc768e48314a8
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-12-17 23:24:11 +0100

    no minimum platf

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Parsers",
-    platforms: [
-        .macOS(.v13)
-    ],
+    // platforms: [
+    //     .macOS(.v13)
+    // ],
     products: [
         .library(
             name: "Parsers",
